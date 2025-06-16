68b93a507e44038df02471d10592df77093af1f8
morso <morso@163.com>
2025-06-16 17:42:18 +0800

    fix action

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 today=`date -u "+%Y-%m-%d"`
-cd arxiv
+
+cd ./arvix
 python paper_filter.py --path ../data/${today}.jsonl
 python paper_translate.py --data ../data/${today}.jsonl
 python paper2md.py --data ../data/${today}_zh.jsonl
 
-ls data/*.jsonl | sed 's|data/||' > assets/file-list.txt
+cd ..
+ls ./data/*.jsonl | sed 's|data/||' > assets/file-list.txt
